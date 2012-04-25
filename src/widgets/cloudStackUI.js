97494ac497b47398adb0019be0f9049e5075606c
bfederle <bfederle@gmail.com>
2012-04-25 16:40:09 -0700

    Setup header area

--- src/widgets/cloudStackUI.js
+++ src/widgets/cloudStackUI.js
@@ -1,7 +1,23 @@
 (function($) {
+  var elems = {
+    header: function() {
+      return $('<div>').attr('id', 'header');
+    },
+
+    logo: function() {
+      return $('<div>').addClass('logo');
+    }
+  };
+  
   $.widget('cloudStackUI.cloudStackUI', {
     _init: function() {
-      
+      var $container = this.element;
+      var $header = elems.header();
+      var $logo = elems.logo();
+
+      $header.append($logo);
+      $container.attr('cloudstack-container', true);
+      $container.append($header);
     }
   });    
 }(jQuery));
--- tests/test.cloudStackUI.js
+++ tests/test.cloudStackUI.js
@@ -3,8 +3,16 @@
 
   test('Basic', function() {
     var $ui = $('<div>').addClass('ui-container');
+    var $header, $logo;
     var ui = {};
 
     ok($ui.cloudStackUI(ui), 'Initialize UI widget');
+    ok($ui.attr('cloudstack-container'), 'Container has cloudstack-container indicator');
+
+    // Check header
+    $header = $ui.find('#header');
+    $logo = $header.find('.logo');
+    equal($header.size(), 1, 'Header present');
+    equal($logo.size(), 1, 'Logo present');
   });
 }(jQuery));
