9643e9da6af49b5da2ae55d07ec3a2cfe95a6402
bfederle <bfederle@gmail.com>
2012-05-04 12:14:07 -0700

    Implement add panel

--- src/widgets/browser.js
+++ src/widgets/browser.js
@@ -1,4 +1,33 @@
 (function($, cloudUI) {
+  var elems = {
+    navigationItem: function(args) {
+      var $li = $('<li>');
+      var $label = $('<span>').html(args.title);
+
+      $li.append($label);
+
+      return $li;
+    },
+    panel: function() {
+      return $('<div>').addClass('panel');
+    }
+  };
+
+  var panel = {
+    add: function(args) {
+      var browser = args.browser;
+      var $panel = elems.panel();
+      var $container = browser.$container;
+      var $navigationList = browser.$navigation.find('ul');
+      var $navigationItem = elems.navigationItem({
+        title: args.title
+      });
+
+      $container.append($panel);
+      $navigationList.append($navigationItem);
+      args.complete($panel);
+    }
+  };
   var makeNavigation = function(args) {
     var browser = args.browser;
     var $navigation = browser.$navigation;
@@ -9,7 +38,17 @@
   cloudUI.widgets.browser = function(args) {
     var browser = {
       $container: args.$container,
-      $navigation: args.$navigation
+      $navigation: args.$navigation,
+
+      addPanel: function(args) {
+        panel.add({
+          browser: browser,
+          complete: args.complete,
+          title: args.title
+        });
+
+        return browser;
+      }
     };
 
     makeNavigation({
--- tests/test.widget.browser.js
+++ tests/test.widget.browser.js
@@ -14,5 +14,40 @@
     ok(browser.$navigation.size(), '$navigation passed in browser object');
     equal($navigation.find('ul').size(), 1, 'Navigation list present');
   });
+
+
+  test('Add panel', function() {
+    var $container = $('<div>');
+    var $navigation = $('<div>');
+    var browser = cloudUI.widgets.browser({
+      $container: $container,
+      $navigation: $navigation
+    });
+
+    stop();
+    browser.addPanel({
+      title: 'test',
+      complete: function($panel) {
+        $panel.append('test contents');
+        start();
+        ok(true, 'addPanel complete called');
+        equal($navigation.find('ul li').size(), 1, 'Navigation item added');
+        equal($container.find('.panel').size(), 1, 'Panel added');
+        equal($container.find('.panel').html(), 'test contents', 'Panel has contents');
+
+        stop();
+        browser.addPanel({
+          title: 'test2',
+          complete: function($panel) {
+            $panel.append('test contents 2');
+            start();
+            equal($navigation.find('ul li').size(), 2, 'Second navigation item added');
+            equal($container.find('.panel').size(), 2, 'Second panel added');
+            equal($container.find('.panel:last').html(), 'test contents 2', 'Second panel has contents');
+          }
+        });
+      }
+    });
+  });
 }(jQuery, cloudUI));
 
