70f210c9de58ac2b0c5ff7e43bd9522c82efae72
Brian Federle <bfederle@gmail.com>
2012-09-13 15:54:04 -0700

    Integrate details widget into application list view
    On click of a list item, open a new panel with a details widget, to
    show fields for the selected item.

--- demos/cloudstack/scripts/cloudStack.js
+++ demos/cloudstack/scripts/cloudStack.js
@@ -19,9 +19,10 @@
       sections: {
         dashboard: { title: 'Dashboard' },
 
-        // Sample list view
         instances: {
           title: 'Instances',
+
+          // List view
           list: {
             id: 'instances',
             fieldDisplay: ['name', 'zone', 'account', 'status'],
@@ -43,9 +44,78 @@
                 });
               }
 
-              setTimeout(function() {
-                args.response.success({ data: data });
-              }, 500);
+              args.response.success({ data: data });
+            }
+          },
+
+          // Detail view
+          details: {
+            tabs: {
+              details: {
+                title: 'Details',
+                fields: [
+                  {
+                    name: { label: 'Name' }
+                  },
+                  {
+                    state: { label: 'State' },
+                    zoneName: { label: 'Zone name' },
+                    account: { label: 'Account' },
+                    domain: { label: 'Domain' },
+                    hypervisor: { label: 'Hypervisor' },
+                    template: { label: 'Template' },
+                    osType: { label: 'OS Type' },
+                    computeOffering: { label: 'Compute offering' },
+                    haEnabled: { label: 'HA Enabled', isBoolean: true },
+                    group: { label: 'Group' }
+                  }
+                ],
+                dataProvider: function(args) {
+                  var instance = args.context.instances[0];
+                  
+                  var data = {
+                    name: instance.name,
+                    state: instance.status,
+                    zoneName: instance.zone,
+                    account: instance.account,
+                    domain: 'users',
+                    hypervisor: 'XenServer',
+                    template: 'CentOS',
+                    osType: 'Linux',
+                    computeOffering: 'Small Instance',
+                    haEnabled: false
+                    
+                    // group: null
+                  };
+
+                  args.response.success({
+                    data: data
+                  });
+                }
+              },
+              statistics: {
+                title: 'Statistics',
+                fields: [
+                  {
+                    totalCPU: { label: 'Total CPU' },
+                    cpuused: { label: 'CPU utilized' },
+                    networkkbsread: { label: 'Network read' },
+                    networkkbswrite: { label: 'Network write' }
+                  }
+                ],
+                dataProvider: function(args) {
+                  var data = {
+                    totalCPU: '500 mhz',
+                    cpuused: '50%',
+                    networkkbsread: '1000 kb/s',
+                    networkkbswrite: '1250 kb/s'
+                  };
+
+                  args.response.success({
+                    data: data
+                  });
+                }
+              }
             }
           }
         },
