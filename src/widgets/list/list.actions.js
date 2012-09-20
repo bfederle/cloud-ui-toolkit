fa4124afad6cde4ee76bf94a70b4f1f92143cd0c
Brian Federle <bfederle@gmail.com>
2012-09-20 14:46:34 -0700

    List actions: add actions column

--- src/widgets/list/list.actions.js
+++ src/widgets/list/list.actions.js
@@ -1,7 +1,20 @@
 (function($, _, cloudUI) {
   cloudUI.event.handler({
+    'list-table-header-row': {
+      init: function(args) {
+        var $tr = args.$tr;
+        var $actionsHeader = $('<th>').addClass('actions').html('Actions');
+
+        $tr.append($actionsHeader);
+      }
+    },
+    
     'list-table-row': {
       init: function(args) {
+        var $tr = args.$tr;
+        var $actions = $('<td>').addClass('actions');
+
+        $tr.append($actions);
       }
     }
   });
